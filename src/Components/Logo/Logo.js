78827c4553224cd4584165b75def0932dce952ef
remon <remosamir443@gmail.com>
2025-05-02 02:32:28 -0700

    first commit

--- src/Components/Logo/Logo.js
+++ src/Components/Logo/Logo.js
@@ -1,7 +1,8 @@
 import React from "react";
 import logo from "../../Images/Logo.svg";
+import { Link } from "react-router-dom";
 const Logo = (props) => {
-  return <img src={logo} alt="Sunmed" width={props.width} className={props.class}/>;
+  return <Link to='/'> <img src={logo} alt="Sunmed" width={props.width} className={props.class}/></Link>;
 };
 
 export default Logo;
